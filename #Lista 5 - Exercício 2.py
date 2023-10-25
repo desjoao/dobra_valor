48649522c4beb56badb5dde06d0c2b147465f72c
desjoao <98698809+desjoao@users.noreply.github.com>
2023-10-25 08:17:22 -0300

    Add files via upload

---
+++ #Lista 5 - Exercício 2.py
@@ -0,0 +1,8 @@
+#Lista 5 - Exercício 2
+
+def DOBRA (x):
+    dobro = x*2
+    return dobro
+
+valor = int(input('Insira um valor inteiro: '))
+print(f'O dobro do valor informado é de {DOBRA(valor)}')
